5bfc61d0e7c921d6e0949ce1d14b30d25db9dde5
Douglas Patterson <douglaspatterson801@gmail.com>
2019-02-08 10:45:50 -0700

    started adding views to storyboard

--- RecoveryConnections/RecoveryConnections/File.swift
+++ RecoveryConnections/RecoveryConnections/File.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable public class RoundButton: UIButton {
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+}
