de514ac3d58653d74a823fa2c785fb96616c1add
Douglas Patterson <douglaspatterson801@gmail.com>
2019-02-21 10:45:59 -0700

    added color to tab bar and its items. added background image to home tab. will leave it there for a while to see how I like it.

---
+++ RecoveryConnections/RecoveryConnections/View/TabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  TabBarController.swift
+//  RecoveryConnections
+//
+//  Created by Douglas Patterson on 2/21/19.
+//  Copyright © 2019 Douglas Patterson. All rights reserved.
+//
+
+import Foundation
